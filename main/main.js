412ce964888939875efb3cb9e1957cc5482f3c02
happyle94 <1351872863@qq.com>
2018-08-15 11:12:29 +0800

    answer

--- main/main.js
+++ main/main.js
@@ -1,3 +1,10 @@
-module.exports = function main() {
+module.exports = function main(input) {
   // Write your code here
+  var num = parseInt (input), result = 0;
+  while (num > 0)
+  {
+	  result += num % 10; //加最右位
+	  num = parseInt (num / 10); //去掉最右位
+  }
+  return result;
 };
