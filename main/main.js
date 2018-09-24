879418f1ccf4e7fccbb2b1be09fcb9e53ad5d4c6
happyle94 <1351872863@qq.com>
2018-09-24 10:47:41 +0800

    my answer

--- main/main.js
+++ main/main.js
@@ -1,10 +1,10 @@
 module.exports = function main(input) {
   // Write your code here
-  var num = parseInt (input), result = 0;
+  var num = parseInt (input), result = 0;//parseInt：解析一个字符串并返回一个整数，result：存放字符串求和的结果
   while (num > 0)
   {
-	  result += num % 10; //加最右位
-	  num = parseInt (num / 10); //去掉最右位
+	  result += num % 10; //把字符串最右边的一位所代表的数字加入结果中
+	  num = parseInt (num / 10); //去掉最右位，即从下一个循环起开始处理更高一位的数字
   }
   return result;
 };
